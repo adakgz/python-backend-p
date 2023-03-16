80102f3ba3f0141b92ee0716a46e5a3f2724bb31
adakgz <121098007+adakgz@users.noreply.github.com>
2023-03-16 20:22:51 +0600

    Create list_for_practice.py

---
+++ list_for_practice.py
@@ -0,0 +1,11 @@
+# List №1
+l = ['Sanjar', 'Tilek', 'Kalys', 'Eldar', 'Elina', 'Beka', 'Elzar', 'Bael', 'Atajan', 'Emir', 'Mamed', 'Beka']
+
+# List №2
+l = ['integer', 'float', 'string', 'list', 'loop', 'tuple', 'while', 'for']
+
+# List №3
+l = [1, 2, 5, 3]
+        
+# String № 1
+s = 'Integers 1,2,3 Floats 44 Strings 5 Lists 10Tuples'
