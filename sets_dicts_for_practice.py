60fbfea5f4ec013096b234df62f35159d01fc63c
adakgz <121098007+adakgz@users.noreply.github.com>
2023-03-17 18:29:18 +0600

    Create sets_dicts_for_practice.py

---
+++ sets_dicts_for_practice.py
@@ -0,0 +1,20 @@
+# Set №1
+{6, 4, 2, 5, 7, 8, 10, 9}
+
+# Set №2
+{'Python', 'it', 'c++', 'java', 'programming'}
+
+# Set №3
+{'html', 'css', 'c++', 'java', 'dart', 'programming'}
+
+# Set №4
+ferma1 = {'корова', 'курица', 'овца', 'заяц'}
+
+# Set №5
+ferma2 = {'корова', 'козел', 'собака', 'заяц'}
+
+# Dictionary 1
+menu = {'manty': 200, 'plov': 150, 'lagman': 130, 'borsh': 100}
+
+# Список 2
+l = [1,2,3,4,5,5,5,67,7,7,8]
