53548c2e0316a28da1be63ea20e968f997d4cd63
adakgz <121098007+adakgz@users.noreply.github.com>
2023-03-28 20:48:31 +0600

    Update practice_files.py

--- practice_files.py
+++ practice_files.py
@@ -4,3 +4,12 @@ Next, install the Python 3 interpreter on your computer. This is the program tha
 you need it before you can do any Python programming. Mac and Linux distributions may include an outdated version of Python (Python 2),
 but you should install an updated one (Python 3). See BeginnersGuide/Download for instructions to download the correct version of Python.
 """
+
+# текст 2
+"""
+.detacilpmoc naht retteb si xelpmoC
+.xelpmoc naht retteb si elpmiS
+.ticilpmi naht retteb si ticilpxE
+.ylgu naht retteb si lufituaeB
+
+"""
